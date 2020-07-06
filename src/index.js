0b5685e17eaa4ef4b34a892c8d12c5d8a65f7cdb
moradov <50326856+moradov@users.noreply.github.com>
2020-07-06 13:15:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 import GloabalState from "../src/context/global/GlobalState";
 const app = (
